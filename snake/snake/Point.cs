7c202fce09cb1ccdc790a7c093f72c4784ccd75d
mahovi4 <mmahovi4@gmail.com>
2021-09-05 16:16:26 +0300

    Class Point

---
+++ snake/snake/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        private char symbol;
+
+        public Point(int x, int y, char symbol)
+        {
+            this.x = x;
+            this.y = y;
+            this.symbol = symbol;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symbol);
+        }
+    }
+}
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,6 +10,13 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(3, 5, '#');
+            p2.Draw();
+
+            Console.ReadLine();
         }
     }
 }
